ec3aedbeb397df99e2b18f6edd69625230510efb
Deepak Kushwaha <deepak2003kushwaha@gmail.com>
2025-03-29 18:35:24 +0530

    script 20

---
+++ 20scr.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#script with argument
+
+echo "first argument is : $1"
+
+echo ""
+
+echo "second argument is : $2"
+
+echo ""
+
+echo "third argument is : $3"
+
+echo ""
+
+echo "fourth argument is : $4"
+
+echo ""
+
+echo "fifth argument is : $5"
+
+echo ""
+
+echo "sixth argument is : $6"
+
+echo ""
+
+echo "total arguments are : $#"
+
+echo ""
+
+echo "all arguments are : $@"
+
+echo "argument using  *: $*"
+
+
